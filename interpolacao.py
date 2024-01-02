cca4d4b8ed00acd28e9ba98ad02454f9208554b3
Victor Magueta <vmagueta7@gmail.com>
2024-01-02 16:10:31 -0300

    implementado os exercicios tabuada e interpolação

---
+++ interpolacao.py
@@ -0,0 +1,30 @@
+email_tmpl = """
+Olá, %(nome)s
+
+Tem interesse em comprar %(produto)s?
+
+Este produto é ótimo para resolver
+%(texto)s
+
+Clique agora em %(link)s
+
+Apenas %(quantidade)d disponiveis!
+
+Preço promocional %(preco).2f
+"""
+
+clientes = ["Maria", "João", "Bruno", "Victor"]
+
+for cliente in clientes:
+    print(
+        email_tmpl
+        %{
+            "nome":cliente,
+            "produto":"caneta",
+            "texto":"Escrever muito bem",
+            "link":"https://canetaslegais.com",
+            "quantidade":1,
+            "preco":50.5
+        }
+    )
+    
---
+++ tabuada.py
@@ -0,0 +1,33 @@
+#! usr/bin/env python
+"""Imprime a tabuada do 1 ao 10.
+
+---Tabuada do 1---
+
+     1 x 1 = 1
+     2 x 1 = 2
+     3 x 1 = 3
+...
+##################
+---Tabuada do 2---
+
+     2 x 1 = 2
+     2 x 2 = 4
+...
+##################
+"""
+__version__ ="0.1.1"
+__author__ = "Victor"
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 7, 9, 10]
+# Iterable (percorriveis)
+numeros = list(range(1,11))
+
+#para cada numero em numeros:
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))        
+    print("#"*18)
+
