553175269edcc46b5c7d9ddd1f9c8cc07f3c7aac
Victor Magueta <vmagueta7@gmail.com>
2024-01-06 19:59:35 -0300

    refatorado o exercício escola com sets para dicionário

---
+++ escola_v3_com_dict.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python3
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por sala
+que frequentam cada uma das atividades.
+"""
+__version__ = "0.1.2"
+
+# Dados
+aulas = {
+    "Inglês": {
+        "sala1": ["Erik","Maia","Joana"],
+        "sala2": ["Carlos","Antonio"]
+    },
+    "Música" : {
+        "sala1": ["Erik"],
+        "sala2": ["Carlos", "Maria"]
+    },
+    "Dança" : {
+        "sala1": ["Gustavo","Sofia", "Joana"],
+        "sala2": ["Antonio"]
+    }
+}
+
+# Listar alunos em cada atividade por sala
+for aula in aulas:
+
+    print(f"Alunos da atividade de {aula}")
+    print("-" * 28)
+    print(f"{aulas[aula]}")
+    print()
