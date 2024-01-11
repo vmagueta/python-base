44bb85a0c223ee27fbf274febe2e51ce968f5599
Victor Magueta <vmagueta7@gmail.com>
2024-01-11 12:54:48 -0300

    Refatorado o Hello Multi Linguas para passar parametros com argumentos através da CLI

--- hello.py
+++ hello.py
@@ -10,26 +10,53 @@ Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
+Ou informe através do CLI argument '--lang'
+
+Ou o usuário terá que digitar.
+
 Execução:
 
     python3 hello.py
     ou
     ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Victor Magueta"
 __license__= "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG","en_US")[:5]
+
+arguments = {"lang": None,"count": 1}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option '{key}'")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
 
 msg = {
+        "en_US" : "Hello, World!",
         "pt_BR" : "Olá, Mundo!",
         "it_IT" : "Ciao, Mondo!",
-        "es_SP" : "Hola, Mundo!",
+        "es_ES" : "Hola, Mundo!",
         "fr_FR" : "Bonjour, Monde!",
 }
 
 
-print(msg[current_language])
+print(
+    msg[current_language]  * int(arguments["count"])
+)
